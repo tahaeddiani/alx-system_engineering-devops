5e7b1c3aa4d3380015c390976ae94ee45b5d562d
Taha ED <73260002+tahaeddiani@users.noreply.github.com>
2023-09-23 01:00:19 +0100

    t0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-#creaetfile
-file {'/tmp/school':
-  mode  => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content   => 'I love Puppet',
+# create a file in /tmp.
+file { '/tmp/school' :
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
